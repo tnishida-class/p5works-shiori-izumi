3b4e50497f7418e18aab137104c3a118f1ca37d2
shiori-izumi <1950594h@stu.kobe-u.ac.jp>
2020-10-22 12:25:59 +0900

    section2-q1
    Create "koteshirabe"

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,2 +1,9 @@
 // 小手調べ
 function setup(){
+    createCanvas(200,200);
+   for(let i=0; i<10; i++){
+    if(i<5){stroke(0, 0, 255)}
+    else {stroke(255,0,0)}
+    noFill();
+    ellipse(120,120,i*5+3);
+  }}
