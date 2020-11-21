8ec24260477521fcab71d18abaf5dcdb22be5aaf
shiori-izumi <1950594h@stu.kobe-u.ac.jp>
2020-11-21 14:44:27 +0900

    change section 3-2

--- section3-2/sketch.js
+++ section3-2/sketch.js
@@ -1,12 +1,13 @@
 // テキスト「関数を作る(1) 引数のある関数」
 // 練習問題：星を描く関数を改造して正N角形を描画する関数を作ってみよう
 function setup(){
-  createCanvas(300, 100);
+  createCanvas(400, 100);
   background(200);
   fill(0);
   crossmark(10, 10, 90, 90);
   ngmark(150, 50, 80);
   star(250, 50, 40);
+  regularPolygon(6,350,50,40);
 }
 
 function crossmark(x1, y1, x2, y2){
@@ -33,4 +34,14 @@ function star(cx, cy, r){
     vertex(x,y);
   }
   endShape(CLOSE);
+}
+function regularPolygon(n, cx, cy, r){
+  beginShape();
+  for(var i = 0; i < n ; i++){
+    let theta = TWO_PI * i  / n - HALF_PI;
+    let x = cx + cos(theta) * r;
+    let y = cy + sin(theta) * r;
+    vertex(x,y);
+  }
+  endShape();
 }
